185744ecc83bc78bc5fa95f62c36954a68892fb5
ves3li <veselin.rafailovic@gmail.com>
2021-06-23 19:41:39 +0200

    adjusting Linked List

--- LinkedList.java
+++ LinkedList.java
@@ -1,4 +1,45 @@
 public class LinkedList {
+
+    private SLNode head;
+
+    //Add an element at the end of the list. 
+    public boolean add(int n){
+        
+        SLNode newNode = new SLNode(n, null);
+        
+        if(head == null){
+            head = newNode;
+            return true;
+        }else{
+
+            SLNode current = head;
+
+            while(current != null){
+                if(current.getData() == newNode.getData()){
+                    return false;
+                }
+                current = current.getNext();
+            }
+
+            return true;
+        }
+
+    }
+
+    public String toString(){
+
+        SLNode current = head;
+        String result = "";
+
+        while(current != null){
+            if(current.getNext() != null){
+                result += current.getData() + "->";
+            }else{
+                result += current.getData();
+            }
+        }
+        return result;
+    }
     
 }
 
--- Test.java
+++ Test.java
@@ -1,5 +1,16 @@
 public class Test{
     public static void main(String[] args) {
-        System.out.println("Hi");
+        
+        LinkedList ls1 = new LinkedList();
+
+        System.out.println(ls1.add(10));
+        System.out.println(ls1.add(5));
+        System.out.println(ls1.add(1));
+        System.out.println(ls1.add(10));
+        System.out.println(ls1.add(20));
+        System.out.println(ls1.add(11));
+        System.out.println(ls1.add(202));
+        System.out.println(ls1.add(1000));
+        System.out.println(ls1.toString());
     }
 }
