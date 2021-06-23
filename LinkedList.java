eaf655c7845f27a25b767f2a37bbf85bcd089dd5
ves3li <veselin.rafailovic@gmail.com>
2021-06-23 19:09:39 +0200

    adjusting node

--- LinkedList.java
+++ LinkedList.java
@@ -4,4 +4,28 @@ public class LinkedList {
 
 class SLNode{
     
+    private int data;
+    private SLNode next;
+
+    public SLNode(int data, SLNode next){
+        this.data = data;
+        this.next = next;
+    }
+
+    public void setData(int data){
+        this.data = data;
+    }
+
+    public void setNext(SLNode next){
+        this.next = next;
+    }
+
+    public int getData(){
+        return this.data;
+    }
+
+    public SLNode getNext(){
+        return this.next;
+    }
+
 }
